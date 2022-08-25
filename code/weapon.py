36553e8a8cc9fb3f4274c98535c200da14f5466c
Winston142O <winston1420@hotmail.com>
2022-08-25 11:41:44 -0500

    Changed some comments

--- code/weapon.py
+++ code/weapon.py
@@ -6,11 +6,11 @@ class Weapon(pygame.sprite.Sprite):
 		self.sprite_type = 'weapon'
 		direction = player.status.split('_')[0]
 
-		# graphic
+		# weapon graphics
 		full_path = f'./graphics/weapons/{player.weapon}/{direction}.png'
 		self.image = pygame.image.load(full_path).convert_alpha()
 		
-		# placement
+		# place weapons in player's and and correct direction
 		if direction == 'right':
 			self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,16))
 		elif direction == 'left': 
